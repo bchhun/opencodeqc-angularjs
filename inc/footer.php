d2a7d0b1a5b1aced3edd30f73b5d2c441a33f860
Bernard Chhun <bernard.chhun@gmail.com>
2012-06-09 14:14:20 -0400

    Ajout dépendances CSSS

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,15 @@
+    <script>
+    var slideshow = new SlideShow();
 
+    var snippets = document.querySelectorAll('.snippet');
+    for(var i=0; i<snippets.length; i++) {
+        new CSSSnippet(snippets[i]);
+    }
+
+    var cssControls = document.querySelectorAll('.css-control');
+    for(var i=0; i<cssControls.length; i++) {
+        new CSSControl(cssControls[i]);
+    }
+    </script>
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -6,13 +6,25 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title>OpenCode Québec - Présentation d'AngularJs</title>
-    <script type="text/javascript" src="js/angular-1.0.0rc10.min.js"></script>
+    
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+
+    <script src="js/prefixfree.min.js"></script>
+    <script src="js/angular-1.0.0rc10.min.js"></script>
+    <script src="slideshow.js"></script>
+    <script src="js/css-edit.js"></script>
+    <script src="js/css-snippets.js"></script>
+    <script src="js/css-controls.js"></script>
+    <script src="js/code-highlight.js"></script>
+
     <link rel="stylesheet" href="css/main.css">
+    <link href="css/slideshow.css" rel="stylesheet" />
+    <link href="css/theme.css" rel="stylesheet" />
+    <link href="css/talk.css" rel="stylesheet" />
 </head>
-<body>
-    <h1>AngularJs</h1>
+<body data-duration="10">
     <!--[if lt IE 7]>
         <p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p>
     <![endif]-->
