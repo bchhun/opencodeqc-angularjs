27a46cfc876feec02d1ab015a3aa3aa4c57ae8a7
bchhun <bchhun@sogix.com>
2012-06-11 11:50:10 -0400

    Supression des fichiers PHP
    Ils ne sont plus nécessaires

--- inc/footer.php
+++
@@ -1,6 +0,0 @@
-    </div>
-    <script type="text/javascript" src="js/restrict-impress.js"></script>
-    <script src="js/impress.js"></script>
-    <script>impress().init();</script>
-</body>
-</html>
